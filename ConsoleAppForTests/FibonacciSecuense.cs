dc708f6ca013f5659346398b489ae16272cd3278
YevhenijGryshkovets <zhenjagryshkovets@gmail.com>
2022-04-30 21:10:41 +0300

    matheFucker - bich

--- ConsoleAppForTests/FibonacciSecuense.cs
+++ ConsoleAppForTests/FibonacciSecuense.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppForTests
     {
         public IEnumerator<int> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
